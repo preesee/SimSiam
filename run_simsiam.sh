2feeba8c65b489483087b54e9512941aaf1f7517
preesee <why123_hua@163.com>
2022-07-02 19:02:00 +0800

    update bash script for support dataset aircrafts

--- run_simsiam.sh
+++ run_simsiam.sh
@@ -2,3 +2,4 @@ export PATH=/home/ubuntu/miniconda3/envs/dl/bin:$PATH
 
 cd /share/huawei/simsiam/
 python main.py  --data_dir /share/huawei/data --log_dir /share/huawei/simsiam/logs/ -c /share/huawei/simsiam/configs/simsiam_cub200.yaml --ckpt_dir ~/.cache/ --hide_progress
+python main.py  --data_dir /share/huawei/data --log_dir /share/huawei/simsiam/logs/ -c /share/huawei/simsiam/configs/simsiam_aircrafts.yaml --ckpt_dir ~/.cache/ --hide_progress
