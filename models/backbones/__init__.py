cc9369f5473b4760a4cf94eeef7647d5fcd68c2f
preesee <why123_hua@163.com>
2022-07-02 18:47:14 +0800

    update for support dataset aircrafts

--- models/backbones/__init__.py
+++ models/backbones/__init__.py
@@ -3,6 +3,7 @@ from .cifar_resnet_2 import ResNet18 as resnet18_cifar_variant2
 
 from .cub_resnet_1 import resnet50 as resnet50_cub200
 from .cub_resnet_1 import resnet50 as resnet50_stanfordcars
+from .cub_resnet_1 import resnet50 as resnet50_aircrafts
 
 
 
